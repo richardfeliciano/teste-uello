9dc1a154adfb08fb0b3d5071c28e3faa492806a2
Richard <richard.feliciano@kuica.com.br>
2021-03-29 08:01:58 -0300

    refactor

--- src/store/delivery/actions.js
+++ src/store/delivery/actions.js
@@ -3,8 +3,6 @@ import axios from '@/services/http.service.js'
 export function fetchDelivery({ commit }) {
   axios
     .get('/deliveries/1')
-    .then((res) => {
-      commit('SET_DELIVERY', res.data)
-    })
+    .then(({ data }) => commit('SET_DELIVERY', data))
     .finally(() => commit('SET_LOADER', false))
 }
